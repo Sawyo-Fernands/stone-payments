deafcc2420e6fcfa8dce416d13a61c0898bc5666
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2023-07-01 21:36:26 -0300

    style[mainPage] : Adicionando layout inicial com imagens

--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -8,6 +8,17 @@ export function MainPage(){
     return(
         <>
         <Header />
+
+        <main className="w-full h-full flex gap-1">
+            <div className="w-1/2 h-full">
+                asdasdasd
+            </div>
+            <div className="w-1/2 h-full flex items-center justify-start">
+                <div>
+                <img src="Graph.svg" alt="" />
+                </div>
+            </div>
+        </main>
         </>
     )
 }
