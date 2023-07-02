464d7986aa724cf5fca25b4a30e9c4c622c1a3f3
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2023-07-01 22:08:14 -0300

    style [ MainPage ] : Criando estilo do header

--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -1,24 +1,20 @@
-'use client'
+"use client";
 
-import { Header } from "@/components/Header"
+import { Header } from "@/components/Header";
 
+export function MainPage() {
+  return (
+    <div className="w-full h-full p-16">
+      <Header />
 
-export function MainPage(){
-
-    return(
-        <>
-        <Header />
-
-        <main className="w-full h-full flex gap-1">
-            <div className="w-1/2 h-full">
-                asdasdasd
-            </div>
-            <div className="w-1/2 h-full flex items-center justify-start">
-                <div>
-                <img src="Graph.svg" alt="" />
-                </div>
-            </div>
-        </main>
-        </>
-    )
+      <main className="w-full h-full flex gap-1">
+        <div className="w-1/2 h-full"></div>
+        <div className="w-1/2 h-full flex items-center justify-start">
+          <div>
+            <img src="Graph.svg" alt="" />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,31 @@
-
-
-export function Header(){
-
-    return(
-        <header>
+export function Header() {
+  return (
+    <header>
+      <div className="flex items-center gap-6">
+        <div>
+          <img src="logo.svg" alt="" />
+        </div>
+        <div className="h-full flex flex-col justify-between gap-1">
+          <div className="flex item-center gap-2 justify-between w-60">
             <div>
-                adasd
+              <span className="font-bold text-gray-600">
+                {"14 de janeiro 2021"}
+              </span>
             </div>
-        </header>
-    )
+            <div>
+              <span className="font-bold text-gray-600">{"|"}</span>
+            </div>
+            <div>
+              <span className="font-bold text-gray-600">{"21:00 UTC"}</span>
+            </div>
+          </div>
+          <div>
+            <span className="text-gray-400">
+              {"Dados de câmbio disponibilizados pela Morningstar."}
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
